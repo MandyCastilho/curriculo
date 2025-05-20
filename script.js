97444e01d20fcfd3218c5130288398a67274aed1
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-05-20 08:40:43 -0300

    Currículo - JS

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 document.getElementById('current-year').textContent = new Date().getFullYear();
 
+  const toggleMode = document.getElementById("toggle-dark-mode");
+
+  toggleMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+
+
+
