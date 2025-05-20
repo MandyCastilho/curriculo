3ff315219ff2bfba812032ab22533d7a85039fbe
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-05-20 08:43:50 -0300

    Currículo - JS

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 document.getElementById('current-year').textContent = new Date().getFullYear();
 
-  const toggleMode = document.getElementById("toggle-dark-mode");
 
-  toggleMode.addEventListener("click", () => {
+  const toggleButton = document.getElementById("toggle-dark-mode");
+  const icon = toggleButton.querySelector(".icon");
+  const text = toggleButton.querySelector(".text");
+
+  // Verifica o localStorage
+  if (localStorage.getItem("tema") === "escuro") {
+    document.body.classList.add("dark-mode");
+    icon.textContent = "☀️";
+    text.textContent = "Modo Claro";
+  }
+
+  toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+    const isDark = document.body.classList.contains("dark-mode");
+
+    icon.textContent = isDark ? "☀️" : "🌙";
+    text.textContent = isDark ? "Modo Claro" : "Modo Escuro";
+
+    localStorage.setItem("tema", isDark ? "escuro" : "claro");
   });
+
+
 
 
 
