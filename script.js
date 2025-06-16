70a53e9b553e6323b8dd92e675dde60836006d1d
Amanda Castilho <165108783+MandyCastilho@users.noreply.github.com>
2025-06-16 08:12:34 -0300

    Currículo - JS

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 document.getElementById('current-year').textContent = new Date().getFullYear();
 
-
   const toggleButton = document.getElementById("toggle-dark-mode");
   const icon = toggleButton.querySelector(".icon");
   const text = toggleButton.querySelector(".text");
 
-  // Verifica o localStorage
-  if (localStorage.getItem("tema") === "escuro") {
-    document.body.classList.add("dark-mode");
-    icon.textContent = "☀️";
-    text.textContent = "Modo Claro";
-  }
+// Verifica o localStorage
+if (localStorage.getItem("tema") === "escuro") {
+  document.body.classList.add("dark-mode");
+  icon.textContent = "☀️";
+  text.textContent = "Modo Claro";
+}
 
-  toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    const isDark = document.body.classList.contains("dark-mode");
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  const isDark = document.body.classList.contains("dark-mode");
 
-    icon.textContent = isDark ? "☀️" : "🌙";
-    text.textContent = isDark ? "Modo Claro" : "Modo Escuro";
+  icon.textContent = isDark ? "☀️" : "🌙";
+  text.textContent = isDark ? "Modo Claro" : "Modo Escuro";
 
-    localStorage.setItem("tema", isDark ? "escuro" : "claro");
-  });
+  localStorage.setItem("tema", isDark ? "escuro" : "claro");
+});
 
 
 
